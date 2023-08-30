dcc9eef980b183224abc3a8bc7f46b01c4811557
ali-haider-381 <143490110+ali-haider-381@users.noreply.github.com>
2023-08-30 08:00:30 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,62 @@
+/*Question number=1*/
+/*Write a program that calculates the area of a rectangle.*/
+
+var sum = 41;
+var temp = sum / (4 + 2) - 5;
+console.log("calculates is", temp);
+var temp = sum + 21 * (11 * 3);
+console.log("calculates is", temp);
+
+
+/*Question number=2*/
+/*Write a program that calculates the percentage*/
+
+var obtainedMarks = 475;
+var totalMarks = 505;
+var result = obtainedMarks / totalMarks * 100;
+console.log("marks percentage is", result);
+
+
+/*Question number=3*/
+/* Write a program that checks if a given number is positive, negative, or zero.*/
+   
+var num = 5;
+
+if (num > 0) {
+    console.log("positive.");
+} else if (num < 0) {
+    console.log("negative.");
+} else {
+    console.log("zero.");
+}
+
+
+/*Question number=4*/
+/*Write a program that checks if a given number is even or odd*/
+
+var num = 5;
+
+if (num % 2 == 0) {
+    console.log("number is even.");
+} else {
+    console.log("number is odd.");
+}
+
+
+/*Question number=5*/
+/*Write a program that determines if a person is eligible to vote based on their age.*/
+
+var age = 18;
+
+if (age >= 18) {
+    console.log("eligible to vote.");
+} else {
+    console.log("not eligible to vote.");
+}
+
+
+/*Question number=6
+/*Write a program that calculates the result of a mathematical expression.	((10 + 5) * 3 - 2) / (4 % 3)  - 7)*/
+
+var result = ((10 + 5) * 3 - 2) / (4 % 3) - 7;
+console.log(result);
